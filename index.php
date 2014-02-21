1affb107a5380fa714e0646a5fa29f918032911f
Johannes Wachter <johannes.wachter@massiveart.com>
2014-02-21 15:16:51 +0100

    added index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the BRO-Project.
+ *
+ * (c) BRO - FHV
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require('autoload.php');
+
+use Example\App;
+
+App::main($argv);
